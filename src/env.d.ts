394c096d11facce6fb494dd73e5a17f59913a92d
pekochan069 <pekochan069@gmail.com>
2025-05-20 11:26:31 +0900

    Add mermeid support

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,9 @@
+import { Mermaid } from "mermaid";
+
+declare var pagefind: unknown | null;
+declare var mermaid: Mermaid | undefined;
+
 interface Window {
   pagefind: unknown | null;
+  mermaid: Mermaid | undefined;
 }
